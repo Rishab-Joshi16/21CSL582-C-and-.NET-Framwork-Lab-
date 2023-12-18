0dbe3faf52f0630f7e2b6cb07b41a670913308b9
Rishab-Joshi16 <119615168+Rishab-Joshi16@users.noreply.github.com>
2023-12-18 19:32:08 +0530

    Add files via upload

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Calc
+{
+    static void Main()
+    {
+        Console.Write("Enter first number:");
+        int a=Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter operator(+,-,*,/,%)");
+        char op= Convert.ToChar(Console.ReadLine()!);
+        Console.Write("Enter second number:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        switch(op)
+        {
+            case '+':Console.WriteLine($"{a} {op} {b} = " + (a + b));
+                     break;
+
+            case '-':Console.WriteLine($"{a} {op} {b} = " + (a - b));
+                break;
+
+            case '*':Console.WriteLine($"{a} {op} {b} = " + (a * b));
+                break;
+
+            case '/':
+                if (b == 0)
+                {
+                    Console.WriteLine("Divide by zero error enter different number for b");
+                }
+                Console.WriteLine($"{a} {op} {b} = " + (a / b));
+                break;
+            case '%':
+                if (b == 0)
+                {
+                    Console.WriteLine("Divide by zero error enter different number for b");
+                }
+                Console.WriteLine($"{a} {op} {b} = " + (a % b));
+                break;
+            default: Console.WriteLine("INvalid operator please try again");
+                    break;
+        }
+
+    }
+}
