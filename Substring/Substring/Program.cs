c742e6d87cf26e320146a838b3b8b7591d095941
Rishab-Joshi16 <119615168+Rishab-Joshi16@users.noreply.github.com>
2024-01-21 20:18:07 +0530

    Add files via upload
    C# code to generate all substrings of a given string using Substring( ) method.

---
+++ Substring/Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Substring
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a string:");
+        string str = Console.ReadLine()!;
+
+        for (int i = 1; i <= str.Length; i++)
+        {
+            for (int j = 0; j <= str.Length - i; j++)
+            {
+                Console.Write(str.Substring(j, i));
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
