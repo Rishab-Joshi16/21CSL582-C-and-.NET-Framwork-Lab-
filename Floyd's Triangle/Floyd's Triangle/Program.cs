4d34639f897412757b6c735b06e8748e62a8c8b5
Rishab-Joshi16 <119615168+Rishab-Joshi16@users.noreply.github.com>
2024-01-21 21:47:19 +0530

    Add files via upload

---
+++ Floyd's Triangle/Floyd's Triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class FLoyd
+{
+    static void Main(string[] args)
+    {
+        int count = 1;
+        Console.Write("Enter no of rows:");
+        int row=Convert.ToInt32(Console.ReadLine());
+        int[,]a=new int[row,row];
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                a[i, j] = count++;
+                Console.Write(a[i, j] + " ");
+            }
+            Console.WriteLine();
+           
+        }
+    }
+}
